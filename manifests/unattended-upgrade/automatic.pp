542d5a382f00f250d2b839b9a4d39948e12937e9
Mads Sülau Valstorp Jørgensen <mads@sulau.dk>
2013-06-28 10:39:28 +0200

    Fixed using dist-codename on older debians

--- manifests/unattended-upgrade/automatic.pp
+++ manifests/unattended-upgrade/automatic.pp
@@ -5,6 +5,25 @@ class apt::unattended-upgrade::automatic inherits apt::unattended-upgrade {
     content => "APT::Periodic::Unattended-Upgrade \"1\";\n",
   }
 
+  # in debian > wheezy, the unattended-upgrades checks o= and a=
+  # in debian < wheezy, it only matches on archivename
+  if $operatingsystem == "Debian" {
+    if $operatingsystemmajrelease >= 7 {
+      $origins = [
+        '${distro_id} ${distro_codename}',
+        '${distro_id} ${distro_codename}-security',
+        '${distro_id} ${distro_codename}-updates',
+      ]
+    } elsif $operatingsystemmajrelease == 6 {
+      $origins = [
+        'Debian oldstable',
+        'Debian oldstable-security',
+      ]
+    } else {
+      fail("Unattended-upgrades not supported by target Debian version")
+    }
+  }
+
   apt::conf{'50unattended-upgrades':
     ensure  => present,
     content => template("apt/unattended-upgrades.${::lsbdistid}.erb"),
