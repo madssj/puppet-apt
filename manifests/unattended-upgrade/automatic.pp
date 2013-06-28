93faee2c3372a3475f9655a28beba02a9c1a110f
Mads Sülau Valstorp Jørgensen <mads@sulau.dk>
2013-06-28 11:02:02 +0200

    Fixed puppet-lint warnings

--- manifests/unattended-upgrade/automatic.pp
+++ manifests/unattended-upgrade/automatic.pp
@@ -7,20 +7,20 @@ class apt::unattended-upgrade::automatic inherits apt::unattended-upgrade {
 
   # in debian > wheezy, the unattended-upgrades checks o= and a=
   # in debian < wheezy, it only matches on archivename
-  if $operatingsystem == "Debian" {
-    if $operatingsystemmajrelease >= 7 {
+  if $::operatingsystem == 'Debian' {
+    if $::operatingsystemmajrelease >= 7 {
       $origins = [
         '${distro_id} ${distro_codename}',
         '${distro_id} ${distro_codename}-security',
         '${distro_id} ${distro_codename}-updates',
       ]
-    } elsif $operatingsystemmajrelease == 6 {
+    } elsif $::operatingsystemmajrelease == 6 {
       $origins = [
         'Debian oldstable',
         'Debian oldstable-security',
       ]
     } else {
-      fail("Unattended-upgrades not supported by target Debian version")
+      fail('Unattended-upgrades not supported by target Debian version')
     }
   }
 
